632af9ab0b148859c24573c45b690e585d0ea717
LukSt92 <lukasz.m.stasiak@gmail.com>
2025-03-14 19:59:18 +0100

    add: Header

--- disney-char-viewer/src/App.jsx
+++ disney-char-viewer/src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { Header } from "./components/Header";
+import styled from "styled-components";
+
+const StyledMainContener = styled.div`
+  display: flex;
+`;
 
 function App() {
-  return <></>;
+  return (
+    <StyledMainContener>
+      <Header />
+    </StyledMainContener>
+  );
 }
 
 export default App;
---
+++ disney-char-viewer/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Switch from "@mui/material/Switch";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import styled from "styled-components";
+
+const StyledHeader = styled.div`
+  width: 100vw;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Header = () => {
+  return (
+    <StyledHeader>
+      <h2>Disney APP</h2>
+      <FormControlLabel control={<Switch />} label="Theme" />
+    </StyledHeader>
+  );
+};
